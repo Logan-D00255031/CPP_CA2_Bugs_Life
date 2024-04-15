0ea978b275e41dfef05151eba2af245ceabf7562
Logan-D00255031 <145552065+Logan-D00255031@users.noreply.github.com>
2024-04-16 00:28:46 +0100

    Added Hopper Class

---
+++ Hopper.cpp
@@ -0,0 +1,85 @@
+//
+// Created by jrush_000 on 15/04/2024.
+//
+
+#include "Hopper.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include "Crawler.h"
+using namespace std;
+
+void Hopper::move() {
+    srand(time(nullptr));   // Generate seed for rand
+    const pair<int, int> current = position;    // log current position
+
+    while (current == position) {   // Loop until Hopper has moved
+        switch (direction) {    // Check Hopper direction
+            case North: {
+                if (isWayBlocked()) {
+                    switch (rand() % 3 + 1) {   // Pick a random direction
+                        default: { direction = East; }
+                        case 3: { direction = South; }
+                        case 4: { direction = West; }
+                    }
+                } else {
+                    if(position.second + hopLength > 9) {
+                        position.second = 9;
+                    } else {
+                        position.second += hopLength; // Move North
+                    }
+                }
+            } break;
+            case East: {
+                if (isWayBlocked()) {
+                    switch (rand() % 3 + 1) {   // Pick a random direction
+                        default: {direction = North;}
+                        case 3: {direction = South;}
+                        case 4: {direction = West;}
+                    }
+                } else {
+                    if(position.first + hopLength > 9) {
+                        position.first = 9;
+                    } else {
+                        position.first += hopLength; // Move East
+                    }
+                }
+            } break;
+            case South: {
+                if (isWayBlocked()) {
+                    switch (rand() % 3 + 1) {   // Pick a random direction
+                        default: {direction = North;}
+                        case 2: {direction = East;}
+                        case 4: {direction = West;}
+                    }
+                } else {
+                    if(position.second - hopLength < 0) {
+                        position.second = 0;
+                    } else {
+                        position.second -= hopLength; // Move South
+                    }
+                }
+            } break;
+            case West: {
+                if (isWayBlocked()) {
+                    switch (rand() % 3 + 1) {   // Pick a random direction
+                        default: {direction = North;}
+                        case 2: {direction = East;}
+                        case 3: {direction = South;}
+                    }
+                } else {
+                    if(position.first - hopLength < 0) {
+                        position.first = 0;
+                    } else {
+                        position.first -= hopLength; // Move West
+                    }
+                }
+            } break;
+        }
+    }
+    path.push_back(position);
+}
+
+Crawler::~Crawler() = default;
---
+++ Hopper.h
@@ -0,0 +1,32 @@
+//
+// Created by jrush_000 on 15/04/2024.
+//
+
+#ifndef HOPPER_H
+#define HOPPER_H
+#include "Bug.h"
+
+
+class Hopper : public Bug {
+private:
+    int hopLength;
+public:
+    Hopper(const int id, const int x, const int y, const Direction direction, const int size, const int hopLength) {
+        this->id = id;
+        this->position.first = x;
+        this->position.second = y;
+        this->direction = direction;
+        this->size = size;
+        this->hopLength = hopLength;
+        alive = true;
+        path.push_back(position);
+    }
+
+    void move() override;
+
+    ~Hopper() override;
+};
+
+
+
+#endif //HOPPER_H
